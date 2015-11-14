685ff5580b37e03c62c2579614aaecd0c44e2c8a
DownLord <DownLord@gmail.com>
2015-11-14 09:01:21 +0000

    update

---
+++ profile.d/prompt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export HOSTSTAGE=${INFLUXDB_HOST/*\./}
+export PROMPT_COMMAND='echo -ne "\033]0;${HOSTNAME}-${HOSTSTAGE}\007"'
+
+case $TERM in
+  screen*)
+    SCREENTITLE='\[\ek\e\\\]\[\ek\u@\h\w\e\\\]'
+    ;;
+  *)
+    SCREENTITLE=''
+    ;;
+esac
+export PS1="${SCREENTITLE}\[\e[1;34m\][\[\e[0;34m\]\h-${HOSTSTAGE}\[\e[1;34m\]]\[\e[1;34m\][\[\e[0;32m\]\w\[\e[1;34m\]]> \[\e[0m\]"
---
+++ profile.d/tabwidth.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+setterm -regtabs 2
